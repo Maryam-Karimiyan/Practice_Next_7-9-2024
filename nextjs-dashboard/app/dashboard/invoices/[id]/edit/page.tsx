5429ade95a35590a69b2aa70fdf433bbf2ca70ee
maryam <Ofogh.maryam14@gmail.com>
2024-07-29 17:15:14 +0330

    chapter 13

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,9 +2,15 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { promise } from "zod";
+import { notFound } from "next/navigation";
+import NotFound from "./notFounf";
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [customers,invoice]=await Promise.all([fetchCustomers(),fetchInvoiceById(id)])
+  if(!invoice){
+    // notFound()
+    return <NotFound/>
+  }
   return (
     <main>
       <Breadcrumbs
